1f25fb744a5c48a1996d3fffccdc06adea528a3d
Josh Lindsay <jlindsay90@gmail.com>
2015-10-28 09:07:04 +0000

    Java Datatypes (java) [4/4] Accepted

--- master/java-datatypes.java
+++ master/java-datatypes.java
@@ -15,12 +15,11 @@ class Solution{
                 System.out.println(x+" can be fitted in:");
                 if(x >= -128 && x <= 127)
                     System.out.println("* byte");
-                if(x > Short.MIN_VALUE && x < Short.MAX_VALUE)
+                if(x >= Short.MIN_VALUE && x <= Short.MAX_VALUE)
                     System.out.println("* short");
-                if(x > Integer.MIN_VALUE && x < Integer.MAX_VALUE)
+                if(x >= Integer.MIN_VALUE && x <= Integer.MAX_VALUE)
                     System.out.println("* int");
-                if(x > Long.MIN_VALUE && x < Long.MAX_VALUE)
-                    System.out.println("* long");
+                System.out.println("* long");
             }
             catch(Exception e)
             {
