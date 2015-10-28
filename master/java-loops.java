548c8f5ba6585069dec990d3f4d51f33ea6bfa79
Josh Lindsay <jlindsay90@gmail.com>
2015-10-28 08:49:18 +0000

    Java Loops (java) [5/5] Accepted

--- master/java-loops.java
+++ master/java-loops.java
@@ -8,16 +8,15 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        s.nextLine();
+        s.nextInt();
         while(s.hasNextInt()) {
             int a = s.nextInt();
             int b = s.nextInt();
             int n = s.nextInt();
             
-            int sum = a;
             for (int i = 0; i < n; ++i) {
-                sum += b * 2^i;
-                System.out.print(sum + (i == n - 1 ? "\n" : " "));
+                a += b * Math.pow(2, i);
+                System.out.print(a + (i == n - 1 ? "\n" : " "));
             }
         }
         s.close();
