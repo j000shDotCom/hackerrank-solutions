3137fa07926a952215ccb6cce1e7c5286f469c7a
Josh Lindsay <jlindsay90@gmail.com>
2015-10-29 21:51:59 +0000

    Time Conversion (java) [5/10] Wrong Answer

--- master/time-conversion.java
+++ master/time-conversion.java
@@ -9,13 +9,10 @@ public class Solution {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String t = s.next();
-        String st;
-        if(t.charAt(8) == 'A') {
-           st = t.substring(0, 8);
-        } else {
-            int i = Integer.parseInt(t.substring(0,2)) + 12;
-            st = i + t.substring(2);
+        int hh = Integer.parseInt(t.substring(0,2)) % 12;
+        if(t.charAt(8) == 'P') {
+           hh += 12;
         }
-        System.out.println(st);
+        System.out.println(hh + t.substring(2, 8));
     }
 }
