f9d13022fb72e6296d6c98d6bcecb88b04a36c95
Josh Lindsay <jlindsay90@gmail.com>
2015-10-29 19:57:28 +0000

    A Very Big Sum (java) [0/2] Wrong Answer

---
+++ master/a-very-big-sum.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        s.nextInt();
+        long sum = 0;
+        while(s.hasNextLong()) {
+            sum += s.nextLong();
+        }
+        s.close();
+    }
+}
