3cec88b388242731e3448fa65e097f584c3a00aa
Josh Lindsay <jlindsay90@gmail.com>
2015-10-29 20:09:23 +0000

    Plus Minus (java) [0/11] Runtime Error

---
+++ master/plus-minus.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int pos = 0, neg = 0, zero = 0;
+        while(s.hasNextInt()) {
+            int a = s.nextInt();
+            if(a < 0) ++neg;
+            else if(a > 0) ++pos;
+            else ++zero;
+        }
+        System.out.printf("%0.4f\n%0.4f\n%0.4f", pos/n, neg/n, zero/n);
+    }
+}
