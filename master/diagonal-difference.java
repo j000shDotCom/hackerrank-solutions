f4e38a0cedd0ca2b76a3475bae215629751c4848
Josh Lindsay <jlindsay90@gmail.com>
2015-10-29 20:05:27 +0000

    Diagonal Difference (java) [0/0] Compilation error

---
+++ master/diagonal-difference.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(Systen.in);
+        int n = s.nextInt();
+        int d0 = 0, d1 = 0;
+        for(int r = 0; r < n; ++r) {
+            int[] nums = new int[n];
+            for(int i = 0; i < n; ++i) {
+                nums[i] = s.nextInt();
+            }
+            d0 += nums[r];
+            d1 += nums[n-r-1];
+        }
+        System.out.println(Math.abs(d0 - d1));
+        s.close();
+    }
+}
