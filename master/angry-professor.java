0b4ac6276a738a4bddbf90834e88bc16c83086de
Josh Lindsay <jlindsay90@gmail.com>
2015-11-14 13:37:44 +0000

    Angry Professor (java8) [12/12] Accepted

--- master/angry-professor.java
+++ master/angry-professor.java
@@ -9,9 +9,9 @@ public class Solution {
         while(in.hasNextInt()) {
             int n = in.nextInt();
             int k = in.nextInt();
-            for (int i = 0; i < n && k >= 0; ++i) {
-                k = in.nextInt() <= 0 ? k - 1 : k;
-            }
+            for (int i = 0; i < n; ++i)
+                if (in.nextInt() <= 0)
+                    --k;
             System.out.println(k <= 0 ? "NO" : "YES");
         }
     }
