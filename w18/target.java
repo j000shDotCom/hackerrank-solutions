d3e060dcd64b7414b2c4dbd54e9be818653f6bbf
Josh Lindsay <jlindsay90@gmail.com>
2015-12-05 02:34:15 +0000

    Target (java8) [51/51] Accepted

--- w18/target.java
+++ w18/target.java
@@ -11,26 +11,41 @@ public class Solution {
         for(int i = 0; i < rings.length; ++i)
             rings[i] = Integer.parseInt(s[i]);
         
-        long sum = 0;
+        Map<Double, Integer> distanceCounts = new TreeMap<>();
         while(in.hasNextInt()) {
-            int x = in.nextInt();
-            int y = in.nextInt();
-            int score = getScore(rings, x, y);
-            //System.out.printf("%d %d %.3f %d\n", x, y, getHypotenuse(x,y), score);
-            sum += score;
+            double hyp = getHypotenuse(in.nextInt(), in.nextInt());
+            incMap(distanceCounts, hyp);
         }
-        System.out.println(sum);
+        System.out.println(calculateScore(rings, distanceCounts));
     }
     
-    private static int getScore(final int[] rings, int x, int y) {
-        double c = getHypotenuse(x, y);
-        if (c >= rings[0])
-            return 0;
-        for(int i = 0; i < rings.length; ++i) {
-            if (c > rings[i])
-                return i;
+    private static void incMap(Map<Double, Integer> map, double d) {
+        if (!map.containsKey(d)) {
+            map.put(d, 1);
+        } else {
+            int count = map.get(d);
+            map.put(d, count + 1);
         }
-        return rings.length;
+    }
+    
+    private static long calculateScore(final int[] rings, Map<Double, Integer> distanceCounts) {
+        long sum = 0;
+        int i = rings.length - 1;
+        for (double d : distanceCounts.keySet()) {
+            boolean scored = false;
+            while (!scored) {
+                if (i < 0 || (i == 0 && d == rings[i])) {
+                    return sum;
+                }
+                if (d <= rings[i]) {
+                    sum += distanceCounts.get(d) * (i + 1);
+                    scored = true;
+                } else {
+                    --i;
+                }
+            }
+        }
+        return sum;
     }
     
     private static double getHypotenuse(int a, int b) {
