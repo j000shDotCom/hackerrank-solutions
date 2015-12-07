4c41a5c32eba262546edbc5865dc012dc20a0199
Josh Lindsay <jlindsay90@gmail.com>
2015-12-07 07:50:49 +0000

    Counter game (java8) [20/20] Accepted

--- master/counter-game.java
+++ master/counter-game.java
@@ -6,24 +6,20 @@ public class Solution {
     
     public static void main(String[] args) {        
         Scanner in = new Scanner(System.in);
-        in.nextInt();
+        in.next();
         while(in.hasNext()) {
-            boolean p1 = thing(new BigInteger(in.next(), 10));
+            boolean p1 = play(new BigInteger(in.next(), 10));
             System.out.println(p1 ? "Louise" : "Richard");
         }
         in.close();
     }
     
-    private static boolean thing(BigInteger n) {
-        return n.bitCount() / 2 == 0;
-    }
-    
     private static boolean play(BigInteger n) {
         boolean p1 = true;
         while(n.compareTo(BigInteger.ONE) >= 0) {
-            //System.out.printf("%30s %10d %10d\n", n.toString(), n.bitCount(), n.bitLength());
+            //System.out.printf("%20s %10d %10d\n", n.toString(), n.bitCount(), n.bitLength());
             if (n.bitCount() == 1)
-                return p1 & (n.bitLength() % 2 == 0);
+                return p1 ^ (n.bitLength() % 2 == 1);
             n = n.clearBit(n.bitLength() - 1);
             p1 = !p1;
         }
