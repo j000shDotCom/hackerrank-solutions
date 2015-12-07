4f8a537c443a7d3f448ebd8997b3a210c968a879
Josh Lindsay <jlindsay90@gmail.com>
2015-12-07 07:24:01 +0000

    Counter game (java8) [7/20] Wrong Answer

--- master/counter-game.java
+++ master/counter-game.java
@@ -1,32 +1,28 @@
 import java.io.*;
 import java.util.*;
+import java.math.BigInteger;
 
 public class Solution {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) {        
         Scanner in = new Scanner(System.in);
         in.nextInt();
-        while(in.hasNextLong()) {
-            boolean turns = play(in.nextLong(), true);
-            System.out.println(turns ? "Loise" : "Richard");
+        while(in.hasNext()) {
+            boolean p1 = play(new BigInteger(in.next(), 10));
+            System.out.println(p1 ? "Louise" : "Richard");
         }
         in.close();
     }
     
-    private static boolean play(long n, boolean currPlayer) {
-        if (n == 1L)
-            return !currPlayer;
-        long p = getPow(n); // TODO could memoize
-        return play((p == 0 ? (n / 2) : (n - p)), !currPlayer);
-    }
-    
-    // [0] n is a power of two
-    // [1,LONG_MAX - LONG_MAX >> 1] next power of two greater than n
-    private static long getPow(long n) {
-        long pow = 1L;
-        while(pow < n) {
-            pow = pow << 1;
+    private static boolean play(BigInteger n) {
+        boolean p1 = true;
+        while(n.compareTo(BigInteger.ONE) >= 0) {
+            //System.out.println(n + " " + n.bitCount() + " " + n.bitLength());
+            if (n.bitCount() == 1)
+                return p1 & (n.bitLength() % 2 == 0);
+            n = n.clearBit(n.bitLength() - 1);
+            p1 = !p1;
         }
-        return pow == n ? 0 : pow >> 1;
+        return !p1;
     }
 }
