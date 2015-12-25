4bd8b7051f5a4caab0a9af298341b4ccc3dd29c4
Josh Lindsay <jlindsay90@gmail.com>
2015-12-25 00:43:59 +0000

    Palindrome Index (java8) [12/15] Processed

---
+++ master/palindrome-index.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        in.nextInt();
+        while(in.hasNext()) {
+            System.out.println(getPalindromeIndex(in.next()));
+        }
+        in.close();
+    }
+    
+    private static int getPalindromeIndex(String str) {
+        int len = str.length();
+        for(int i = 0; i < len / 2; ++i) {
+            int j = len - i - 1;
+            char a = str.charAt(i);
+            char b = str.charAt(j);
+            if (a == b)
+                continue;
+            char nextb = str.charAt(j - 1);
+            if (a != nextb)
+                return i;
+            return j;
+        }
+        return -1;
+    }
+}
