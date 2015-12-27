c60486e3575e9ba2f8399ad11649a73e74c13f50
Josh Lindsay <jlindsay90@gmail.com>
2015-12-27 09:42:52 +0000

    Evaluating e^x (haskell) [1/6] Processed

---
+++ master/eval-ex.hs
@@ -0,0 +1,17 @@
+solve :: Double -> Double
+solve x = foldl (\ acc n -> acc + (power x n / (fromIntegral $ factorial n))) 0.0 [0..10]
+
+power :: (Num a) => a -> Int -> a
+power x n
+    | n == 0 = 1
+    | otherwise = x * power x (n - 1)
+
+factorial :: Int -> Int
+factorial = (facts !!)
+
+facts :: [Int]
+facts = 1 : zipWith (*) facts [1..]
+
+main :: IO ()
+main = getContents >>= mapM_ print. map solve. map (read::String->Double). tail. words
+
