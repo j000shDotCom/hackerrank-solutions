80fbe07d19c9bb32e40bf931b06cb94765c70c0a
Josh Lindsay <jlindsay90@gmail.com>
2015-12-27 10:30:28 +0000

    Computing the GCD (haskell) [10/10] Accepted

---
+++ master/functional-programming-warmups-in-recursion---gcd.hs
@@ -0,0 +1,16 @@
+module Main where
+
+gcd' :: Integral a => a -> a -> a
+gcd' a b
+    | b == 0 = a
+    | otherwise = gcd b $ mod a b
+
+-- This part is related to the Input/Output and can be used as it is
+-- Do not modify it
+main = do
+  input <- getLine
+  print . uncurry gcd' . listToTuple . convertToInt . words $ input
+ where
+  listToTuple (x:xs:_) = (x,xs)
+  convertToInt = map (read :: String -> Int)
+
