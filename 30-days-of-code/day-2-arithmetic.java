2d9c6fe5c45aa835bb19cb82b34d69707b5dcf23
Josh Lindsay <jlindsay90@gmail.com>
2016-01-02 18:37:08 +0000

    Day 2: Arithmetic! (java8) [4/4] Accepted

--- 30-days-of-code/day-2-arithmetic.java
+++ 30-days-of-code/day-2-arithmetic.java
@@ -13,9 +13,9 @@ public class Arithmetic {
       int T = sc.nextInt(); // tip percentage
       int X = sc.nextInt(); // tax percentage
       
-      double tip = (T * M) / 100;
-      double tax = (X * M) / 100;
-      int total = (int) Math.round((M + tip + tax) * 100) / 100;
+      double tip = T * M / 100.0;
+      double tax = X * M / 100.0;
+      int total = (int) Math.round(M + tip + tax);
       
       System.out.println("The final price of the meal is $" + total + ".");
       
