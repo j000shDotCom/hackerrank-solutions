65c448c741aa803832c340b179866d4509f163f3
Josh Lindsay <jlindsay90@gmail.com>
2016-01-06 20:28:03 +0000

    Find A Substring (java8) [2/10] Wrong Answer

---
+++ master/find-substring.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.nextLine();
+        StringBuilder sb = new StringBuilder();
+        while (n-- > 0)
+            sb.append(in.nextLine()).append('\n');
+        
+        String str = sb.toString().trim();
+        
+        sb = new StringBuilder();
+        n = in.nextInt();
+        in.nextLine();
+        
+        while (n-- > 0)
+            sb.append(in.nextLine()).append('\n');
+        
+        String subStr = sb.toString().trim();
+        
+        int count = 0;
+        Pattern pattern = Pattern.compile("[\\w]" + subStr + "[\\w]");
+        Matcher m = pattern.matcher(str);
+        while (m.find()) {
+            count += 1;
+        }
+        
+        System.out.println(count);
+    }
+}
