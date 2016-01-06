928f4da25993a7d7f0d530fde5be7bbd20281aed
Josh Lindsay <jlindsay90@gmail.com>
2016-01-06 09:28:53 +0000

    Matching Start & End (java8) [6/6] Accepted

--- master/matching-start-end.java
+++ master/matching-start-end.java
@@ -3,7 +3,7 @@ public class Solution {
     public static void main(String[] args) {
         
         Regex_Test tester = new Regex_Test();
-        tester.checker("^\\d\\w{4}\\.");
+        tester.checker("^\\d\\w{4}\\.$");
     
     }
 }
