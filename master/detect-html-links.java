304baf48d031f71d9c6d61fb203a8fdaa528d1e9
Josh Lindsay <jlindsay90@gmail.com>
2016-01-06 18:49:54 +0000

    Detect HTML links (java8) [8/10] Wrong Answer

---
+++ master/detect-html-links.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        in.nextLine();
+        StringBuilder sb = new StringBuilder();
+        while (in.hasNextLine()) {
+            sb.append(in.nextLine());
+            sb.append(' ');
+        }
+        String str = sb.toString().trim();
+        Pattern pattern = Pattern.compile("href=\"([^\"]*)\"[^>]*>([^<]*)</a>");
+        Matcher m = pattern.matcher(str);
+        while (m.find()) {
+            System.out.println(m.group(1) + "," + m.group(2));
+        }
+    }
+}
