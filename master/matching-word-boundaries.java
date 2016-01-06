dd3543d2595b3f11eccd9bb0146dc002e054bddc
Josh Lindsay <jlindsay90@gmail.com>
2016-01-06 10:16:12 +0000

    Matching Word Boundaries (java8) [7/7] Accepted

---
+++ master/matching-word-boundaries.java
@@ -0,0 +1,6 @@
+public class Solution {
+    public static void main(String[] args) {
+        Regex_Test tester = new Regex_Test();
+        tester.checker("\\b[aeiouAEIOU][a-zA-Z]*\\b");
+    }
+}
