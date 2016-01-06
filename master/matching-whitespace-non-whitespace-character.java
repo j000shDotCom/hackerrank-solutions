71c6067aee6177da5e9f16e357fcf37950250e41
Josh Lindsay <jlindsay90@gmail.com>
2016-01-06 09:31:35 +0000

    Matching Whitespace & Non-Whitespace Character (java8) [6/6] Accepted

---
+++ master/matching-whitespace-non-whitespace-character.java
@@ -0,0 +1,9 @@
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("\\S{2}\\s\\S{2}\\s\\S{2}");
+    }
+}
+
