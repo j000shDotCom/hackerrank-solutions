4a7cef014e1aea66c304e790e87eeaec074a0db3
Josh Lindsay <jlindsay90@gmail.com>
2016-01-06 10:02:34 +0000

    Matching {x, y} Repetitions (java8) [8/8] Accepted

--- master/matching-x-y-repititions.java
+++ master/matching-x-y-repititions.java
@@ -1,6 +1,6 @@
 public class Solution {
     public static void main(String[] args) {
         Regex_Test tester = new Regex_Test();
-        tester.checker("^\\d{1,2}[a-zA-Z]{3,}.{0,3}$");
+        tester.checker("^\\d\\d?[a-zA-Z]{3,}\\.{0,3}$");
     }
 }
