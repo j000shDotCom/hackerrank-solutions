54c4009f88631c892b8817177bf1f6b0477a3a46
Josh Lindsay <jlindsay90@gmail.com>
2016-01-06 09:50:39 +0000

    Matching Range Of Characters (java8) [7/7] Accepted

--- master/matching-range-of-characters.java
+++ master/matching-range-of-characters.java
@@ -3,7 +3,7 @@ public class Solution {
     public static void main(String[] args) {
         
         Regex_Test tester = new Regex_Test();
-        tester.checker("^[a-z][0-9][^a-z][^A-Z][A-Z]");
+        tester.checker("^[a-z][1-9][^a-z][^A-Z][A-Z]");
     
     }
 }
