34d5b857bfca8c4c3f448ac0ca370126fe4bc8a6
Josh Lindsay <jlindsay90@gmail.com>
2016-01-08 10:00:22 +0000

    Restricted Repetitions (java) [33/33] Accepted

--- regular-expresso/restricted-repetitions.java
+++ regular-expresso/restricted-repetitions.java
@@ -1,8 +1,10 @@
 public class Solution {
     public static void main(String[] args) {
         Regex_Test tester = new Regex_Test();
-        String back = "(cc)+k(kk){2,10}$";
-        // "hh((hh)+|h(hhh)+|hhh(h{5})+|h{47})aaa(a(aa)+|(aaa)+|aa(a{5})+|a{47})(cc)+k(kk){2,10}$"
-        tester.checker("(h{2,})\\1+(a{2,})\\2+" + back);
+        String h = "(h{2,})\\1+";
+        String a = "(a{3,})\\2+";
+        String c = "(cc)+";
+        String k = "k(kk){2,10}";
+        tester.checker("^" + h + a + c + k + "$");
     }
 }
