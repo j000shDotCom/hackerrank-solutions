59b4aa29486a1b02221b1a3282e96c3eb12bd75b
Josh Lindsay <jlindsay90@gmail.com>
2016-01-08 06:16:58 +0000

    Balanced Strings (java) [19/37] Wrong Answer

--- regular-expresso/balanced-strings.java
+++ regular-expresso/balanced-strings.java
@@ -3,6 +3,6 @@ public class Solution {
     public static void main(String[] args) {
         
         Regex_Test tester = new Regex_Test();
-        tester.checker("(ab|ba|aabb|bbaa)*");
+        tester.checker("(ab|ba)*");
     }
 }
