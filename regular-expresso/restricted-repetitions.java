aaf86928888a2a61ae4c524bea6d9555e8afbd30
Josh Lindsay <jlindsay90@gmail.com>
2016-01-08 06:59:58 +0000

    Restricted Repetitions (java) [26/33] Wrong Answer

---
+++ regular-expresso/restricted-repetitions.java
@@ -0,0 +1,8 @@
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("h{4,}a{6,}(cc)+k{5,21}$");
+    }
+}
