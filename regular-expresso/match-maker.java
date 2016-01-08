1fc35ee665f0217e5d366731f2149b394a8d16ac
Josh Lindsay <jlindsay90@gmail.com>
2016-01-08 13:07:10 +0000

    Match Maker (java) [40/42] Wrong Answer

--- regular-expresso/match-maker.java
+++ regular-expresso/match-maker.java
@@ -3,6 +3,7 @@ public class Solution {
     public static void main(String[] args) {
         
         Regex_Test tester = new Regex_Test();
-        tester.checker("^(d[bcgj]|[ej][^h]|[hi][bhe]|b(e|hd))|a(a[adg]|b[cf])$");
+        // "^(d[bcgj]|[ej][^h]|[hi][bhe]|b(e|hd))|a(a[adg]|b[cf])$"
+        tester.checker("^([dej][^dhi]|[hi][bhe]|b(e|hd))|a(a[adg]|b[cf])$");
     }
 }
