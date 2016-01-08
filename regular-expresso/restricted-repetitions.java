429a5688b45574e1fd4f52fedfa62d2265df77e8
Josh Lindsay <jlindsay90@gmail.com>
2016-01-08 09:30:36 +0000

    Restricted Repetitions (java) [25/33] Wrong Answer

--- regular-expresso/restricted-repetitions.java
+++ regular-expresso/restricted-repetitions.java
@@ -1,7 +1,7 @@
 public class Solution {
     public static void main(String[] args) {
         Regex_Test tester = new Regex_Test();
-        // "hh((hh)+|h((hhh)+|hhh{5}+))aaa((a(aa)+|aa{5}+)|(aaa)+)(cc)+k(kk){2,10}$"
-        tester.checker("hh((hh)+|h((hhh)+|hhh{5}+))aaa((a(aa)+|aa{5}+)|(aaa)+)(cc)+k(kk){2,10}$");
+        // "hh((hh)+|h((hhh)+|hh(h{5})+))aaa((a(aa)+|a(a{5})+)|(aaa)+)(cc)+k(kk){2,10}$"
+        tester.checker("(h{2})+(a{2})+(cc)+k(kk){2,10}$");
     }
 }
