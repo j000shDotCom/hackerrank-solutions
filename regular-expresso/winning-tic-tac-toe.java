d8e6a661a52a1e7cf59b489acc4b0705bf027a31
Josh Lindsay <jlindsay90@gmail.com>
2016-01-08 11:46:45 +0000

    Winning Tic Tac Toe (java) [30/41] Wrong Answer

--- regular-expresso/winning-tic-tac-toe.java
+++ regular-expresso/winning-tic-tac-toe.java
@@ -15,8 +15,9 @@ public class Solution {
         ..X .X. X..
         */
         Regex_Test tester = new Regex_Test();
-        String horizontal = "^(...){0,2}([XO])\\2\\2"; // "(...)?(...)?([XO])\\3{2}"
-        String vertdiag = "([XO]).{1,3}\\3.{1,3}\\3";
-        tester.checker(horizontal + "|" + vertdiag);
+        String horizontal = "^(...){0,2}(X|O)\\2\\2";
+        String vertical = "(X|O)((.|..|...)\\3){2}";
+        // String vertdiag = "(X|O).{1,3}\\3.{1,3}\\3";
+        tester.checker(horizontal + "|" + vertical);
     }
 }
