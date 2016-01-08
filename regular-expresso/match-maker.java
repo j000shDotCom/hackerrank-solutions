92f4d1e5392491a563a2b7da20919d3916b9476c
Josh Lindsay <jlindsay90@gmail.com>
2016-01-08 12:52:53 +0000

    Match Maker (java) [38/42] Wrong Answer

---
+++ regular-expresso/match-maker.java
@@ -0,0 +1,8 @@
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^(d[bcdj]|e[ae]|[hi][bhe]|j[af]|b(e|hd))|aa(a[adg]|b[cf])$");
+    }
+}
