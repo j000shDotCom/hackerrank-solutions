33655ada72c82669d67155edc3212b8b1762743a
Josh Lindsay <jlindsay90@gmail.com>
2016-01-08 09:34:34 +0000

    Restricted Repetitions (java) [30/33] Wrong Answer

--- regular-expresso/restricted-repetitions.java
+++ regular-expresso/restricted-repetitions.java
@@ -2,6 +2,6 @@ public class Solution {
     public static void main(String[] args) {
         Regex_Test tester = new Regex_Test();
         // "hh((hh)+|h((hhh)+|hh(h{5})+))aaa((a(aa)+|a(a{5})+)|(aaa)+)(cc)+k(kk){2,10}$"
-        tester.checker("(h{2})+(a{2})+(cc)+k(kk){2,10}$");
+        tester.checker("hh((hh)+|h(hhh)+|hhh(h{5})+)aaa(a(aa)+|(aaa)+|aa(a{5})+)(cc)+k(kk){2,10}$");
     }
 }
