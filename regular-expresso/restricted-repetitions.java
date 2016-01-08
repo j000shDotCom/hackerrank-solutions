fee226c66adc410e15434346cc294f636c9cb1f2
Josh Lindsay <jlindsay90@gmail.com>
2016-01-08 07:30:02 +0000

    Restricted Repetitions (java) [27/33] Wrong Answer

--- regular-expresso/restricted-repetitions.java
+++ regular-expresso/restricted-repetitions.java
@@ -1,8 +1,6 @@
-public class Solution {    
-
+public class Solution {
     public static void main(String[] args) {
-        
         Regex_Test tester = new Regex_Test();
-        tester.checker("hh(hh|h{5})+aaaa(aa|a{5})+(cc)+(kk){2,10}k$");
+        tester.checker("hh(hh|h{3})+aaaa(aa|a{3})+(cc)+(kk){2,10}k$");
     }
 }
