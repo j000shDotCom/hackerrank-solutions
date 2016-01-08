b6f707c9defc2653ead3b6d16048002b43174407
Josh Lindsay <jlindsay90@gmail.com>
2016-01-08 07:27:05 +0000

    Restricted Repetitions (java) [28/33] Wrong Answer

--- regular-expresso/restricted-repetitions.java
+++ regular-expresso/restricted-repetitions.java
@@ -3,6 +3,6 @@ public class Solution {
     public static void main(String[] args) {
         
         Regex_Test tester = new Regex_Test();
-        tester.checker("h{4}(hh|h{3})*a{6,}(aa|a{3})*(cc)+(kk){2,10}k$");
+        tester.checker("hh(hh|h{5})+aaaa(aa|a{5})+(cc)+(kk){2,10}k$");
     }
 }
