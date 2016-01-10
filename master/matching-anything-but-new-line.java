19ddf73d5875362a1933382763f20a3ecfb8a84b
Josh Lindsay <jlindsay90@gmail.com>
2016-01-10 21:23:45 +0000

    Matching Anything But a Newline (java) [3/3] Accepted

--- master/matching-anything-but-new-line.java
+++ master/matching-anything-but-new-line.java
@@ -3,7 +3,7 @@ public class Solution {
     public static void main(String[] args) {
         
         Regex_Test tester = new Regex_Test();
-        tester.checker(".{3}\\..{3}\\..{3}\\..{3}"); 
+        tester.checker("...(\\....){3}"); 
     
     }
 }
