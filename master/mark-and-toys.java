9da027841464d7c61292d350bad214ba3cea9592
Josh Lindsay <jlindsay90@gmail.com>
2016-01-12 20:55:51 +0000

    Mark and Toys (java8) [4/16] Wrong Answer

---
+++ master/mark-and-toys.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        long k = in.nextLong();
+        long[] prices = new long[n];
+        
+        int i = 0;
+        while(in.hasNextLong())
+            prices[i++] = in.nextLong();
+        Arrays.sort(prices);
+        
+        i = 0;
+        while(k > 0 && i < prices.length && k > prices[i])
+            k -= prices[++i];
+        System.out.println(i);
+    }
+}
