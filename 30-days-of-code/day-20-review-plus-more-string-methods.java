d2e3de5ec73588a431deb8474d3c230607850855
Josh Lindsay <jlindsay90@gmail.com>
2016-01-27 01:47:33 +0000

    Day 20: Review + More String Methods! (java8) [6/11] Wrong Answer

---
+++ 30-days-of-code/day-20-review-plus-more-string-methods.java
@@ -0,0 +1,18 @@
+    
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String[] sarr = in.nextLine().split("[\\s'.,?!_@+]+");
+        System.out.println(sarr.length);
+        for(String s : sarr)
+            System.out.println(s);
+    }
+}
+
