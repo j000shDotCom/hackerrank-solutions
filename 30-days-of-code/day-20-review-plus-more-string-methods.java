e89fc6ae83c9c79539faf2797e57ad4a3c5e2681
Josh Lindsay <jlindsay90@gmail.com>
2016-01-27 01:59:19 +0000

    Day 20: Review + More String Methods! (java8) [11/11] Accepted

--- 30-days-of-code/day-20-review-plus-more-string-methods.java
+++ 30-days-of-code/day-20-review-plus-more-string-methods.java
@@ -9,10 +9,14 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String[] sarr = in.nextLine().split("[\\s'.,?!_@+\\[\\]\\\\]+");
-        System.out.println(sarr.length);
-        for(String s : sarr)
-            System.out.println(s);
+        try {
+            String[] sarr = in.nextLine().trim().split("[\\s'.,?!_@+\\[\\]\\\\]+");
+            System.out.println(sarr.length);
+            for(String s : sarr)
+                System.out.println(s);
+        } catch (Exception e) {
+            System.out.println(0);
+        }
     }
 }
 
