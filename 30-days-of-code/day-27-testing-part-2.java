00be1f31f9847582c9a926dd8642b3fe940dc6dc
Josh Lindsay <jlindsay90@gmail.com>
2016-01-29 23:39:49 +0000

    Day 27: Testing Part II! (java8) [1/1] Accepted

--- 30-days-of-code/day-27-testing-part-2.java
+++ 30-days-of-code/day-27-testing-part-2.java
@@ -2,18 +2,17 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        in.nextInt();
-        while (in.hasNextInt()) {
-            int n = in.nextInt();
-            int t = in.nextInt();
-            while(n > 0) {
-                if (in.nextInt() <= 0)
-                    t--;
-            }
-            System.out.println(t >= 0 ? "YES" : "NO");
-        }
+        System.out.println("5"); 
+        System.out.println("10 9"); 
+        System.out.println("-50 0 64 -14 -56 -91 -65 -36 -51 28"); 
+        System.out.println("4 1"); 
+        System.out.println("0 1 3 -6"); 
+        System.out.println("9 6"); 
+        System.out.println("58 29 35 18 43 -28 -76 43 0"); 
+        System.out.println("11 5"); 
+        System.out.println("-6 88 17 -96 43 83 99 -25 0 -39 -86"); 
+        System.out.println("6 4"); 
+        System.out.println("74 9 -14 -13 14 0");
     }
 }
