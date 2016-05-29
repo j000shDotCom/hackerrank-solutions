e486aac58b6419570eafeaf5880da9aa73a800cb
Josh Lindsay <jlindsay90@gmail.com>
2016-05-29 00:45:55 +0000

    Non-Divisible Subset (python3) [7/16] Wrong Answer

--- w20/non-divisible-subset.py
+++ w20/non-divisible-subset.py
@@ -2,12 +2,21 @@ n,k = input().strip().split(' ')
 n,k = [int(n),int(k)]
 ns = [int(a_temp) for a_temp in input().strip().split(' ')]
 
-ms = set()
+mod = {}
 for n in ns:
-    found = False
-    for m in ms:
-        if not found and (n + m) % k == 0:
-            found = True
-    if not found:
-       ms.add(n)
-print(len(ms))
+    m = n % k
+    if m not in mod:
+        mod[m] = []
+    mod[m].append(n)
+
+count = 0
+ms = mod.keys()
+for m in ms:
+    if k - m in ms:
+        count += max(len(mod[m]), len(mod[k-m]))
+        mod[m] = []
+        mod[k-m] = []
+    else:
+        count += len(mod[m])
+print(count)
+
